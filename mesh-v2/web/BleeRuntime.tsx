d36bed6136583fa3506ea41e2a69c95d8856f8da
Rish <75270803+rishantster@users.noreply.github.com>
2026-09-11 18:28:30 +0530

    Normalize Mesh runtime component import

--- mesh-v2/web/BleeRuntime.tsx
+++ mesh-v2/web/BleeRuntime.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from 'react';
 import { Capacitor, registerPlugin, type PluginListenerHandle } from '@capacitor/core';
-import BleeApp from '../../src/components/BleeApp';
+import BleeApp from './BleeApp';
 
 type MeshStatus = {
   running: boolean;
